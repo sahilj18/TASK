65f7160050bb0628517959a278206d8328a301d8
sahilj18 <sahilmernj18@proton.me>
2024-10-23 08:01:26 +0000

    save

--- src/main.js
+++ src/main.js
@@ -1,19 +1,2 @@
 import './index.css'
 
-// make a function
-const input =[1,2,3,4,5]
-
-function randomvalue(array){
-    const list=[...array];
-    return() =>{
-        const rn=Math.floor(Math.random()*array.length);
-        const removed= list.splice(rn,1)[0]
-        return removed
-        
-    }
-       
-}
-
-
-
-
